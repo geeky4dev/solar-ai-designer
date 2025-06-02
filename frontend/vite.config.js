423adab9cd4e999264c0ecde76ec55043fae070b
geeky4dev <geeky4dev@gmail.com>
2025-06-02 13:16:21 +0200

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['some-module'],  // aquí pones el nombre del módulo externo
-    },
-  },
+//  build: {
+//    rollupOptions: {
+//      external: ['some-module'],  // aquí pones el nombre del módulo externo
+//    },
+//  },
   server: {
     proxy: {
       '/ai': 'http://localhost:5000',
